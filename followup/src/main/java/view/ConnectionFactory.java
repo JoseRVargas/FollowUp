b3f73e555bef79528d41ba5a4e3667213f1935e7
JoseVargas <92692967+JoseRVargas@users.noreply.github.com>
2021-11-06 17:38:58 -0300

    Add files via upload

---
+++ followup/src/main/java/view/ConnectionFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jose Luiz
+ */
+public class ConnectionFactory {
+    
+    private static String servername = "localhost";
+    private static String dbname = "users_db";
+    private static String usuario = "root";
+    private static Integer porta = 3306;
+    private static String senha = "";
+    
+    public static Connection getConnection(){
+        Connection cnx = null;
+        
+        MysqlDataSource datasource = new MysqlDataSource();
+        
+        datasource.setServerName(servername);
+        datasource.setUser(usuario);
+        datasource.setPassword(senha);
+        datasource.setDatabaseName(dbname);
+        datasource.setPort(porta);
+        
+        try {
+            cnx = datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(" Get connection -> " + ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return cnx;
+    }
+    
+}
